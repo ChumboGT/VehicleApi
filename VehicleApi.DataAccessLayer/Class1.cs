8890987d23afc49c7d797964a059c375f25defe6
Tim Forste <timfor@saif.com>
2021-10-23 22:42:21 -0700

    Initial DBContext for InMemoryProvider

--- VehicleApi.DataAccessLayer/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace VehicleApi.DataAccessLayer
-{
-    public class Class1
-    {
-    }
-}
---
+++ VehicleApi.DataAccessLayer/InMemoryProvider/VehicleDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace VehicleApi.DataAccessLayer.InMemoryProvider
+{
+    public class VehicleDbContext : DbContext
+    {
+        public VehicleDbContext(DbContextOptions<VehicleDbContext> options)
+            : base(options)
+        {  }
+
+        public DbSet<>
+    }
+}
