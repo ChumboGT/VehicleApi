558b69da857570ae193862b4b41e55e78a75fe87
Tim Forste <timfor@saif.com>
2021-10-24 20:08:54 -0700

    Add-Migration and Update-Database results from SQLite

---
+++ VehicleApi.DataAccessLayer/Migrations/20211025021703_Initial.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VehicleApi.DataAccessLayer.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int32", nullable: false)
+                        .Annotation("Devart.Data.SQLite:Autoincrement", true),
+                    Year = table.Column<int>(type: "int32", nullable: false),
+                    Make = table.Column<string>(type: "text", nullable: true),
+                    Model = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+        }
+    }
+}
