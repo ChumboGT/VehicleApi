d35d06552cd4f29e88e34503480819f1971b54c3
Tim Forste <timfor@saif.com>
2021-10-24 19:24:31 -0700

    Update folder structure and namespace for SQLite

---
+++ VehicleApi.DataAccessLayer/SQLite/VehicleDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using VehicleApi.Entities.Models;
+
+namespace VehicleApi.DataAccessLayer.SQLite
+{
+    public class VehicleDbContext : DbContext
+    {
+        private const string DbPath = "VehicleDB.db";
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+        public VehicleDbContext(DbContextOptions<VehicleDbContext> options)
+            : base(options)
+        {  }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        { 
+            options.UseSQLite($"Data Source={DbPath}");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Vehicle>().ToTable("Vehicles");
+        }
+    }
+}
