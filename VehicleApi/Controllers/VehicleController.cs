6e2db51111320f1ee93190c5883c716497769695
Tim Forste <timfor@saif.com>
2021-10-24 21:19:31 -0700

    Setup DI in controller

--- VehicleApi/Controllers/VehicleController.cs
+++ VehicleApi/Controllers/VehicleController.cs
@@ -3,15 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VehicleApi.Business.Interfaces;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace VehicleApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class VehicleController : ControllerBase
     {
+        IVehicleService _vehicleService;
+
+        public VehicleController(IVehicleService vehicleService)
+        {
+            _vehicleService = vehicleService;
+        }
+
         // GET: api/<VehicleController>
         [HttpGet]
         public IEnumerable<string> Get()
